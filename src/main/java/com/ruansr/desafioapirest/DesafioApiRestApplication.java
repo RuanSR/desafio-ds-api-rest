ac7929ba933d01765de6d59b354f504fcefabbcf
RuanSR <ruansilvarosa@gmail.com>
2024-04-28 13:23:16 -0300

    Estrutura Inicial

---
+++ src/main/java/com/ruansr/desafioapirest/DesafioApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.ruansr.desafioapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruansr/desafioapirest/DesafioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruansr.desafioapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
